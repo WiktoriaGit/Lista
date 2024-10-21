28012d13b3d459c9e3aedd6ff1ab99f6c55a61f6
WiktoriaGit <wiktoriagithub@gmail.com>
2024-10-21 21:35:53 +0200

    Dokumentacja doxygen i LaTeX

--- ListaDwukierunkowa/DoublyLinkedList.h
+++ ListaDwukierunkowa/DoublyLinkedList.h
@@ -1,35 +1,88 @@
-#pragma once
+﻿#pragma once
 
 #include "Node.h"
 
 using namespace std;
 
+/**
+ * \brief Klasa DoublyLinkedList
+ *
+ * Klasa ta reprezentuje Listę dwukierunkową
+ * Zawiera element Node, który wskazuje head listy.
+ *
+ */
+
 class DoublyLinkedList {
 
     private:
-        Node* head;
+        Node* head;             /**< Zmienna która będzie przechowywać naszą zawartość węzła */
 
     public:
+
+        /**
+         * Konstruktor Klasy.
+         * Ustawia head jako nullptr.
+         */
         DoublyLinkedList();
 
+        /**
+         * Wstawianie węzła na początku listy
+         * 
+         * @param value wartość która będzie przypisana do węzła.
+         */
         void insertAtStart(int value);
 
+        /**
+         * Wstawianie węzła na końcu listy
+         *
+         * @param value wartość która będzie przypisana do węzła.
+         */
         void insertAtEnd(int value);
 
+        /**
+         * Wstawianie węzła we wskazany indks
+         *
+         * @param value wartość która będzie przypisana do węzła.
+         * @param pos pozycja na którą ma być wstawiony węzeł
+         */
         void insertIndex(int value, int pos);
 
+        /**
+         * Usunięcie węzła z początku listy
+         */
         void deleteAtStart();
 
+        /**
+         * Usunięcie węzła z końca listy
+         */
         void deleteAtEnd();
 
+        /**
+         * Usunięcie węzła z konkretnego indeksu
+         *
+         * @param pos pozycja która ma zostać usunięta z listy.
+         */
         void deleteIndex(int pos);
 
+        /**
+         * Wyświetlenie całej listy
+         */
         void showList();
 
+        /**
+         * Wyświetlenie listy w odwrotnej kolejności
+         */
         void showReverseList();
 
+
+        /**
+         * Wyświetlenie listy w odwrotnej kolejności
+         */
         void clearList();
 
+        /**
+         * Destruktor klasy
+         */
         ~DoublyLinkedList();
 };
 
--- ListaDwukierunkowa/Node.h
+++ ListaDwukierunkowa/Node.h
@@ -1,12 +1,24 @@
-#pragma once
+﻿#pragma once
+
+/**
+ * \brief Klasa Node
+ *
+ * Klasa ta reprezentuje węzeł w liście.
+ * Zawiera zawartość oraz wskaźniki na kolejne elementy.
+ *
+ */
 
 class Node {
 
     public:
-        int value;
-        Node* nextNode;
-        Node* prevNode;
+        int value;          /**< Zmienna która będzie przechowywać naszą zawartość węzła */
+        Node* nextNode;     /**< Wskaźnik na następny element listy */
+        Node* prevNode;     /**< Wskaźnik na poprzedni element listy */
 
+        /**
+         * Konstruktor Klasy
+         * @param value wartość która będzie przypisana do węzła.
+         */
         Node(int value);
 };
 
